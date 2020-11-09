cb1939cd165cc4213762e1ad2b23bfeed1a638f3
Samieh Jarabba <abdels6375@gmail.com>
2020-11-09 13:27:39 -0600

    update
    adding an image literal as well as taking away the opacity, it looked wierd

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
     }
 
 
